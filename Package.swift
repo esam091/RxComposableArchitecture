8a6aa52bfd735a45e3c12e15692ebc2fd79a608c
Samuel Edwin <esam091@gmail.com>
2020-06-04 08:51:30 +0700

    Add third-party dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RxComposableArchitecture",
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -12,15 +13,18 @@ let package = Package(
             targets: ["RxComposableArchitecture"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.1.1"),
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxComposableArchitecture",
-            dependencies: []),
+            dependencies: [
+                .product(name: "CasePaths", package: "swift-case-paths"),
+                .product(name: "RxCocoa", package: "RxSwift")
+        ]),
         .testTarget(
             name: "RxComposableArchitectureTests",
             dependencies: ["RxComposableArchitecture"]),
